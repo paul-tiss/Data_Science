27eb50e4d942dc551d820cb2d4187c71ca182987
paul-tiss <91611906+paul-tiss@users.noreply.github.com>
2022-11-05 02:27:16 +0100

    tri polluants

---
+++ Python/type_polluant.py
@@ -0,0 +1,130 @@
+# Le but est de classer tout ces polluants selon le critère suivant:
+
+# Il vaut 1 si il n'y a aucun risque pour la santé
+# Il vaut 3 si il est interdit par l'eu ou la france
+# Et 2 sinon
+
+
+# Je te conseille de chercher direct sur wikipedia le nom des polluants c'est marqué direct si c'est interdit ou les dangers pour la santé
+
+[
+'1,1,1-trichloroéthane (TCE)'
+ "1,2-dichloroéthane (DCE - chlorure d'éthylène)" '1,3-butadiène'
+ '1,4-dioxane' 'Acide cyanhydrique (HCN)'
+ 'Acétaldéhyde (aldéhyde acétique \x96 éthanal)'
+ 'Aldéhyde formique (formaldéhyde)'
+ 'Aluminium et composés (exprimés en tant que Al)' 'Ammoniac (NH3)'
+ 'Anthracène' 'Antimoine et composés (exprimés en tant que Sb)'
+ 'Arsenic et composés (exprimés en tant que As)' 'Azote total' 'Benzène'
+ 'Cadmium et composés (exprimés en tant que Cd)'
+ 'Carbone organique total (en tant que C total ou DCO/3)'
+ 'Chlore et composés inorganiques (en tant que HCl)'
+ 'Chloro-alkanes (C10-C13)' 'Chlorofluorocarbures (CFC)'
+ 'Chloroforme (trichlorométhane)' 'Chlorométhane (chlorure de méthyle)'
+ 'Chlorure de vinyle (chloroéthène)' 'Chlorures (en tant que Cl total)'
+ 'Chrome et composés (exprimés en tant que Cr)'
+ 'Chrome hexavalent et composés (exprimés en tant que Cr VI)'
+ 'Cobalt et composés (exprimés en tant que Co)'
+ 'Composés organiques volatils non méthaniques (COVNM)'
+ 'Composés organohalogénés (exprimés en tant que AOX)'
+ 'Cuivre et composés (exprimés en tant que Cu)'
+ 'Cyanures (sous forme de CN total)'
+ 'Demande biologique en oxygène (DBO5)'
+ 'Demande chimique en oxygène (DCO)' 'Di(2-éthylhexyle)phtalate (DEHP)'
+ 'Dichlorométhane (DCM, chlorure de méthylène)'
+ 'Dioxines et furanes (PCDD + PCDF) (exprimés en iTeq)'
+ "Dioxyde de carbone (CO2) d'origine biomasse" 'Diuron'
+ 'Epichlorhydrine (1-chloro-2,3-époxypropane)'
+ 'Etain et composés (exprimés en tant que Sn)'
+ 'Fer et composés (exprimés en tant que Fe)'
+ 'Fluor et composés inorganiques (en tant que HF)' 'Fluoranthène'
+ 'Fluorures (en tant que F total)' 'Hexachlorobenzène (HCB)'
+ 'Hexachlorobutadiène (HCBD)' '1,2,3,4,5,6-hexachlorocyclohexane (HCH)'
+ 'Hexafluorure de soufre (SF6)' 'Hydrazine' 'Hydrocarbures'
+ 'Hydrocarbures aromatiques polycycliques (HAP)'
+ 'Hydrochlorofluorocarbures (HCFC)' 'Hydroflurocarbures (HFC)'
+ 'Isoproturon' 'Manganèse et composés (exprimés en tant que Mn)'
+ 'Matières en suspension (MES)'
+ 'Mercure et composés (exprimés en tant que Hg)'
+ 'Monoxyde de carbone (CO)' 'Méthane (CH4)' 'Méthanol (alcool méthylique)'
+ 'Naphthalène' 'Nickel et composés (exprimés en tant que Ni)'
+ 'Nonylphénols et éthoxylates de nonylphénol (NP/NPE)'
+ "Octylphénols et éthoxylates d'octylphénol" "Oxyde d'éthylène"
+ 'Oxydes de soufre (SOx/SO2)' "Oxydes d'azote (NOx/NO2)"
+ 'Particules (PM10)' 'Pentachlorobenzène' 'Perfluorocarbures (PFC)'
+ 'Plomb et composés (exprimés en tant que Pb)' 'Poussières totales'
+ "Sulfure d'hydrogène (H2S)"
+ 'Titane et composés (exprimés en tant que Ti)' 'Trichloréthylène'
+ 'Tétrachlorométhane (TCM, tétrachlorure de carbone)'
+ 'Zinc et composés (exprimés en tant que Zn)'
+ "Dioxyde de carbone (CO2) total (d'origine biomasse et non biomasse)"
+ 'Halons' 'Biphényles polychlorés (PCB)' '1,1,2,2-tetrachloroéthane'
+ 'Benzo (g, h, i) pérylène' 'Toluène'
+ 'Xylènes (somme ortho-xylène, méta-xylène, para-xylène)'
+ 'Sulfonate de perfluorooctane (SPFO)' 'Vanadium et ses composés'
+ 'Thallium et ses composés (exprimés en tant que Tl)'
+ 'PCBi : somme des 7 PCB indicateurs (PCB 28+PCB 52+PCB 101+PCB 118+PCB 138+PCB 153+PCB 180)'
+ "Hydroxyde d'ammonium (NH4+)" 'Sélénium']
+
+
+
+Résultat doit resembler à ça
+[valeur1,valeur2,.....valeur97]
+[3,
+ 3, 2,
+ 2,  2,
+ 2, 
+ 2,
+ 1, 1,
+ 2, 1,
+ 2, 1, 2,
+ 2,
+ 1,
+ 1,
+ 3, 1, 
+ 2, 2,
+ 2, 1,
+ 2,
+ 2,
+ 1,
+ 2,
+ 0,
+ 1,
+ 2,
+ 0,
+ 0, 3,
+ 3,
+ 2,
+ 2, 3,
+ 2,
+ 1,
+ 1,
+ 2, 2,
+ 2, 3,
+ 2, 2,
+ 1, 2, 1,
+ 2,
+ 0, 1,
+ 2, 1,
+ 0,
+ 2,
+ 2, 1, 2,
+ 2, 2,
+ 2,
+ 1, 2,
+ 1, 2,
+ 0, 3, 2,
+ 2, 0,
+ 2,
+ 1, 3,
+ 3,
+ 2,
+ 2,
+ 0, 2, 3,
+ 2, 2,
+ 2,
+ 2, 2,
+ 2,
+ 0,
+ 0,2
+ ]
